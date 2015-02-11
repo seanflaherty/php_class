59ed35fb9152b47abef8dde9a294868de7583b49
Sean Flaherty <sean.flaherty@gmail.com>
2015-02-10 20:30:11 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Page</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+                $siteName = "PHP Page";
+                echo $siteName;
+            ?>
+        </h1>
+        <p><?php echo "Hello World!"?></p>
+
+        <h2>Quotes</h2>
+        <?php
+        echo '"I can\'t find it" he said.  "Here it is" she said.';
+        ?>
+        <br >
+        <?php
+            echo "I can't do it.  ";
+            echo "Yes you can!";
+
+        ?>
+
+        <h2>Store</h2>
+        <?php
+        $product1 = 11;
+        echo "Price Product 1: $$product1<br >";
+        $product2 = 12;
+        echo "Price Product 2: $$product2<br >";
+        $product3 = 13;
+        echo "Price Product 3: $$product3<br >";
+        echo "<br >";
+        echo "<br >";
+        $product1 +=5;
+        echo "Price Product 1: $$product1<br >";
+        $product2 -=3;
+        echo "Price Product 2: $$product2<br >";
+        $product3 *=2;
+        echo "Price Product 3: $$product3<br >";
+
+
+
+        $average = ($product1 + $product2 + $product3)/3;
+
+
+
+      echo "<p>
+          Average:  $average</p>"
+      ?>
+
+
+
+
+
+
+    </body>
+</html>
